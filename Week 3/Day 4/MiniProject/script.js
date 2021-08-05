83cee62bc3497a001fd82ef6c3e2e27942fa209e
Sasha <deadsnake555@gmail.com>
2021-08-05 12:36:53 +0300

    MiniProject
    Todo list miniproject

--- Week 3/Day 4/MiniProject/script.js
+++ Week 3/Day 4/MiniProject/script.js
@@ -1,4 +1,7 @@
 let field = document.getElementById("field");
+let field2 = document.getElementById("field2");
+let field3 = document.getElementById("field3");
+let field4 = document.getElementById("field4");
 let button = document.getElementById("add");
 let area = document.getElementById("tasks");
 let del = document.getElementById("del");
@@ -6,10 +9,36 @@ let arr = [];
 let task;
 //function for the add button to take the info from the field
 
+
+
+
+
+
+field.addEventListener('keypress', activate);
+field2.addEventListener('keypress', activate);
+field3.addEventListener('keypress', activate);
+field4.addEventListener('keypress', activate);
+
+function activate(e){
+    if(e.keyCode === 13){
+        adding()
+    }
+};
+
 function adding(){
     let value = field.value;
-    if (value.length <= 0){
-        alert("You need to type a task first")
+    let value2 = field2.value;
+    let value3 = field3.value.split("-");
+    let value4 = field4.value.split("-");
+    let restday = value4[2] - value3[2];
+    day = value3[2];
+    month = value3[1];
+    year = value3[0];
+    day2 = value4[2];
+    month2 = value4[1];
+    year2 = value4[0];
+    if (value.length <= 0 || value2.length <= 0){
+        alert("Please fill up the fields")
     }
     else{
         console.log(value);
@@ -18,21 +47,60 @@ function adding(){
     console.log(arr);
     task.setAttribute("id", arr.indexOf(value));
     task.innerHTML = `${value}
+                    <span class="more" ><p id ='titl'>Description</p>${value2}<br>
+                    <p id ='titl'>Starting Date</p>${day}-${month}-${year}<br>
+                    <p id ='titl'>Ending Date</p>${day2}-${month2}-${year2}<br>
+                    <p id ='titl'>Remaining Days</p>${restday}</span>
+
     <button id="del" onclick="deleting(event)">x</button>
-    <input type="checkbox" id="check"></input>`; 
+    <input type="checkbox" id="check"></input>
+    <button class='rdmore'>More...</button>`; 
+    
     area.appendChild(task);
+    let rdbtn = document.getElementsByClassName('rdmore');
+    
+    console.log(rdbtn);
+
+    rdbtn[arr.indexOf(value)].addEventListener('click', add);
+
+    function add(e){
+        e.target.parentNode.children[0].classList.toggle('more');
+    };
+
+    const taskdet = {
+        [field.value] : field2.value
+    };
+    localStorage.setItem('Task', JSON.stringify(taskdet));
     field.value = '';
+    field2.value = '';
+    field3.value = '';
+    field4.value = '';
     };
     
 };
 
 function deleting(e){
-    e.target.parentNode.remove();
-    arr.shift();
+    
+    var checkin = confirm('Do you want to delete the selected task ?')
+    if( checkin == true){
+        e.target.parentNode.remove();
+        arr.shift();
+    }
+    else{
+        null;
+    }
+    
 };
 
 function deletall(){
-    arr = [];
-    area.innerHTML = "";
+    var checkinall = confirm('Do you want to delete all the tasks ?')
+    if(checkinall == true){
+        arr = [];
+        area.innerHTML = "";
+    }
+    else{
+        null;
+    }
+    
 }
 
---
+++ Week 6/Day 1/DATABASESQL/script.js
@@ -0,0 +1,6 @@
+let form1 = document.getElementById('key');
+let form2 = document.getElementById('value');
+let btn1 = document.getElementById('btn');
+let btn2 = document.getElementById('btn2')
+let res = document.getElementsByClassName('result');
+
